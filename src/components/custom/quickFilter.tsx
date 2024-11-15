8dc7ceee69ffc97ae35c074bcc08039e8b82f74e
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-15 14:58:56 +0100

    quick filter element

---
+++ src/components/custom/quickFilter.tsx
@@ -0,0 +1,27 @@
+import { ChatCircle, ShareFat, ThumbsUp } from "@phosphor-icons/react/dist/ssr";
+
+interface CampaignCardProps {
+  title: string;
+  isSelected: boolean;
+}
+
+const QuickFilter = ({ title, isSelected }: CampaignCardProps) => {
+  // text color
+  const textColor = isSelected ? "text-green-500" : "text-gray-500";
+
+  // border color
+  const borderColor = isSelected ? "border-green-500" : "border-gray-500";
+
+  // bg color
+  const backgroundColor = isSelected ? "bg-green-100" : "bg-gray-100";
+
+  return (
+    <div
+      className={`!text-mm ${textColor} border ${borderColor} ${backgroundColor} py-1 px-2 rounded-lg w-fit`}
+    >
+      {title}
+    </div>
+  );
+};
+
+export default QuickFilter;
