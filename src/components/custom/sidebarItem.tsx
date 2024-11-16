8d190891cf5dc1387a0b6f7194e090b5064d2213
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-16 11:17:52 +0100

    added the href and active property

--- src/components/custom/sidebarItem.tsx
+++ src/components/custom/sidebarItem.tsx
@@ -1,17 +1,41 @@
-import { IconProps } from "@phosphor-icons/react";
+"use client";
+
+import { IconProps } from "@phosphor-icons/react/";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface SidebarItemProps {
   icon: React.ElementType<IconProps>;
   text: string;
+  href: string;
 }
 
-export const SidebarItem = ({ icon: Icon, text }: SidebarItemProps) => {
+export const SidebarItem = ({ icon: Icon, text, href }: SidebarItemProps) => {
+  const pathname = usePathname();
+  const isActive = pathname == href;
+
   return (
-    <Link href={""}>
-      <div className="flex items-center gap-3 rounded-lg p-2 hover:bg-green-500 group hover:text-white">
-        <Icon size={20} className="text-gray-500 group-hover:text-white" />
-        <p className="!text-mm text-gray-500 group-hover:text-white">{text}</p>
+    <Link href={href}>
+      <div
+        className={`flex items-center gap-3 rounded-lg p-2 ${
+          isActive
+            ? "bg-green-500 text-white"
+            : "hover:bg-green-500 group hover:text-white"
+        }`}
+      >
+        <Icon
+          size={20}
+          className={`${
+            isActive ? "text-white" : "text-gray-500 group-hover:text-white"
+          }`}
+        />
+        <p
+          className={`!text-mm ${
+            isActive ? "text-white" : "text-gray-500 group-hover:text-white"
+          }`}
+        >
+          {text}
+        </p>
       </div>
     </Link>
   );
