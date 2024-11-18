b56fac4d1e6fb76450bde271d01c1beb5b9e30af
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-11-18 08:27:22 +0100

    added PIN regex

--- src/schema/verify.ts
+++ src/schema/verify.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
 export const verifySchema = z.object({
-  pin: z.string().min(6, { message: "OTP must be 6 characters" }),
+  pin: z
+    .string()
+    .min(6, { message: "OTP must be 6 characters" })
+    .regex(/^\d+$/, { message: "PIN must only contain numbers" }),
 });
