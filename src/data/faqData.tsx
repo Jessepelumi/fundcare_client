90e19e88cbadfbd4befb25124230701087afd4fe
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-02-06 14:20:06 +0100

    faq questions and responses

---
+++ src/data/faqData.tsx
@@ -0,0 +1,82 @@
+export const faqData = [
+  {
+    category: "Campaigns",
+    questions: [
+      {
+        question: "How do I create a campaign on Fund Care?",
+        answer:
+          "Navigate to the 'Campaigns' section and click on 'Start a Campaign'. Follow the on-screen instructions to set up your campaign details, funding goals, and upload relevant media. Once complete, submit it for review.",
+      },
+    ],
+  },
+  {
+    category: "Donations",
+    questions: [
+      {
+        question: "How can I make a donation to a project?",
+        answer:
+          "Visit the 'Community' section, select the project you wish to support, and click on the 'Donate' button. You can choose the donation amount and preferred payment method before confirming your donation.",
+      },
+    ],
+  },
+  {
+    category: "Tax",
+    questions: [
+      {
+        question: "Is my donation tax-deductible?",
+        answer:
+          "Yes, most donations made through Fund Care are tax-deductible. After completing your donation, you will receive a receipt via email, which you can use for tax purposes. However, tax deductibility may vary depending on your location and local laws.",
+      },
+    ],
+  },
+  {
+    category: "Impact",
+    questions: [
+      {
+        question: "How do I track the impact of my donations?",
+        answer:
+          "You can track the progress and impact of your donations by visiting the 'Impact Report' section on your dashboard. Here, you'll find updates from project creators, including reports, photos, and milestones achieved.",
+      },
+    ],
+  },
+  {
+    category: "Payment",
+    questions: [
+      {
+        question: "What payment methods are supported?",
+        answer:
+          "Fund Care supports the use of bank cards, bank transfers, USSD, and mobile payments (flutterwave, Paystack)",
+      },
+    ],
+  },
+  {
+    category: "Password",
+    questions: [
+      {
+        question: "How can I reset my password?",
+        answer:
+          "Go to the login page and click on 'Forgot Password'. Enter your registered email address, and we'll send you instructions to reset your password. If you don't receive the email, check your spam folder or contact support. You can also reset your password by visiting the 'Settings' section on your dashboard.",
+      },
+    ],
+  },
+  {
+    category: "Edit",
+    questions: [
+      {
+        question: "Can I edit my campaign after it's been published?",
+        answer:
+          "Yes, you can edit certain details of your campaign like the description, images, and updates. However, key information such as the funding goal and campaign title may require approval or might not be editable after publishing.",
+      },
+    ],
+  },
+  {
+    category: "Support",
+    questions: [
+      {
+        question: "How do I contact customer support?",
+        answer:
+          "You can contact our customer support team by emailing support@fundcare.com or using the live chat feature on our website. We aim to respond within 24 hours on business days.",
+      },
+    ],
+  },
+];
