188f6a13e366cb24d9a23544ff3d5a1a242e3247
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-02-06 14:05:31 +0100

    added faqs

--- src/app/(dashboard)/support/page.tsx
+++ src/app/(dashboard)/support/page.tsx
@@ -2,6 +2,13 @@ import * as React from "react";
 
 import { CaretRight, MagnifyingGlass } from "@phosphor-icons/react/dist/ssr";
 import { Input } from "@/components/ui/input";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { faqData } from "@/data/faqData";
 
 export default function Support() {
   return (
@@ -16,7 +23,7 @@ export default function Support() {
 
       <div className="flex h-full overflow-hidden w-full">
         <section className="flex flex-col overflow-y-auto pb-3 w-full">
-          <div className="flex items-center border rounded-lg pl-2 w-1/3">
+          <div className="flex items-center border rounded-lg pl-2 sm:w-2/3 md:w-1/2 lg:w-1/3">
             <MagnifyingGlass size={22} className="text-gray-500" />
             <Input
               type="text"
@@ -31,13 +38,42 @@ export default function Support() {
             Popular Frequently Asked Questions (FAQs)
           </h3>
 
+          <Accordion
+            type="single"
+            collapsible
+            className="border px-3 rounded-xl"
+          >
+            {faqData.map((section, index) => (
+              <React.Fragment key={index}>
+                {section.questions.map((faq, idx) => {
+                  const isLastItem =
+                    index === faqData.length - 1 &&
+                    idx === section.questions.length - 1;
+
+                  return (
+                    <AccordionItem
+                      value={`item-${index}-${idx}`}
+                      key={idx}
+                      className={isLastItem ? "border-none" : ""}
+                    >
+                      <AccordionTrigger className="!text-mm hover:no-underline hover:text-gray-500">
+                        {faq.question}
+                      </AccordionTrigger>
+                      <AccordionContent className="!text-mm">
+                        {faq.answer}
+                      </AccordionContent>
+                    </AccordionItem>
+                  );
+                })}
+              </React.Fragment>
+            ))}
+          </Accordion>
+
           <div className="pb-3"></div>
 
           <h3 className="!text-mm text-gray-500 pb-2">Contact Support</h3>
           <div className="flex items-center justify-between w-full border p-3 rounded-xl">
-            <p className="text-gray-500 text-mm">
-              Send us an email: support@fundcare.com
-            </p>
+            <p className="text-mm">Send us an email: support@fundcare.com</p>
             <CaretRight size={14} weight="bold" />
           </div>
         </section>
