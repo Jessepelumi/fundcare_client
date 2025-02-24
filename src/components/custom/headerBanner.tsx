17a0e3473888c6d521dfec4a731e108036ce7dcd
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-02-24 17:52:48 +0100

    added functionality to hide banner

--- src/components/custom/headerBanner.tsx
+++ src/components/custom/headerBanner.tsx
@@ -1,29 +1,58 @@
+"use client";
+
 import { CaretRight, X } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
+import { useState } from "react";
+import { Button } from "../ui/button";
 
 interface HeaderBannerProps {
-  link?: string;
+  backgroundImage: string;
   title: string;
   subtitle?: string;
+  link?: string;
   href?: string;
+  color: string;
 }
 
-const headerBanner = ({ link, title, subtitle, href }: HeaderBannerProps) => {
+const HeaderBanner = ({
+  backgroundImage,
+  title,
+  subtitle,
+  link,
+  href,
+  color,
+}: HeaderBannerProps) => {
+  const [isBannerShown, setIsBannerShown] = useState(true);
+
   return (
-    <header className="flex justify-between rounded-xl p-3 bg-campaign-background bg-cover">
-      <div>
-        {link && href && (
-          <Link href={href} className="flex gap-3 items-center">
-            <span className="!text-mm text-white">{link}</span>
-            <CaretRight size={14} weight="bold" className="text-white" />
-          </Link>
-        )}
-        <h2 className="text-lg text-white">{title}</h2>
-        {subtitle && <p className="!text-mm text-white">{subtitle}</p>}
+    <header className={`${isBannerShown ? "block" : "hidden"}`}>
+      <div
+        className="flex justify-between rounded-xl p-3 bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div>
+          {link && href && (
+            <Link href={href} className="flex gap-3 items-center">
+              <span className="!text-mm text-white">{link}</span>
+              <CaretRight size={14} weight="bold" className="text-white" />
+            </Link>
+          )}
+          <h2 className="text-lg text-white">{title}</h2>
+          {subtitle && <p className="!text-mm text-white">{subtitle}</p>}
+        </div>
+        <Button
+          variant="ghost"
+          size="icon"
+          className={`text-${color} hover:text-black`}
+          onClick={() => setIsBannerShown(!isBannerShown)}
+        >
+          <X weight="bold" />
+        </Button>
       </div>
-      <X weight="bold" className="text-white" />
+
+      <div className="pb-4"></div>
     </header>
   );
 };
 
-export default headerBanner;
+export default HeaderBanner;
